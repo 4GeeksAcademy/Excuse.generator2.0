eb060ebfa8401b61066c5f5596923d7c2daa6d06
Emdihoy <136965828+Emdihoy@users.noreply.github.com>
2023-10-06 19:19:13 +0000

    Nombres de variables mejoradas, funciones simplificadas

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-// my arrays:
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+// Arrays which i'll get the random element from:
 let who = ["El perro ", "Su novia ", "La chavala ", "El guevón "];
 let action = ["se comió ", "tiró ", "desintegró ", "se fumó "];
 let what = ["mi tarea ", "las llaves ", "el coche ", "la casa "];
@@ -9,19 +14,18 @@ let when = [
   "mientras comia.",
   "cuando salí."
 ];
-
 // My random reusable function:
-function obtenerElemento(cualquier) {
-  return Math.floor(Math.random() * cualquier.length);
+function getFinalItem(arrayAleatorio) {
+  return arrayAleatorio[Math.floor(Math.random() * arrayAleatorio.length)];
 }
 //My reload restart function for random items
-window.onload = function excusesrc() {
-  var excusesrc =
-    who[obtenerElemento(who)] +
-    action[obtenerElemento(action)] +
-    what[obtenerElemento(what)] +
-    when[obtenerElemento(when)];
+window.onload = function excuseSrc() {
+  var excuseSrc =
+    getFinalItem(who) +
+    getFinalItem(action) +
+    getFinalItem(what) +
+    getFinalItem(when);
   //my link to the html source id
   var excuse = document.getElementById("excuse");
-  excuse.innerHTML = excusesrc;
+  excuse.innerHTML = excuseSrc;
 };
